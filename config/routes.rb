c197491d2c264578bc659a1543e4772c6ae1947c
sumangbcn <suman@onlinecatering.es>
2016-10-24 17:22:20 +0200

    solved devise issues and added stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  as :users do
-    get 'signin' => 'devise/sessions#new'
+ 
+  
+  devise_scope :user do
+    get "/signin" => "devise/sessions#new"
     delete 'signout' => 'devise/sessions#destroy'
     get 'signup' => 'devise/registrations#new'
   end
+
   root 'pages#home'
   get  'about' => 'pages#about' 
   get  'contact' => 'pages#contact-us'
